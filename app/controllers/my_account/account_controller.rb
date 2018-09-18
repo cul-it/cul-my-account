61ca42e585bedf779469a318b3813f8506bf494b
Matt Connolly <mjc12@cornell.edu>
2018-09-18 17:13:07 -0400

    Add  first table of checkouts

--- app/controllers/my_account/account_controller.rb
+++ app/controllers/my_account/account_controller.rb
@@ -12,13 +12,21 @@ module MyAccount
     def show
       netid = 'mjc12'
       @patron = get_patron_info netid
+      @checkouts = get_patron_checkouts netid
     end
 
     def get_patron_info netid
-      uri = URI.parse("https://lstools.library.cornell.edu/patrons/patron_info_service.cgi/netid/#{netid}")
+      uri = URI.parse("#{ENV['MY_ACCOUNT_PATRONINFO_URL']}/#{netid}")
       response = Net::HTTP.get_response(uri)
       record = JSON.parse(response.body)
       record[netid]
+    end
+
+    def get_patron_checkouts netid
+      uri = URI.parse("#{ENV['MY_ACCOUNT_ILSAPI_URL']}?netid=#{netid}")
+      response = Net::HTTP.get_response(uri)
+      record = JSON.parse(response.body)
+      record['items']
     end
 
   end
