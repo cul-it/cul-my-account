d2fd52c0a5e558c39515d45d18f7e83e93e92baf
Matt Connolly <mjc12@cornell.edu>
2018-09-19 11:55:02 -0400

    Add method handlers to deal with functions from main app

--- app/helpers/my_account/application_helper.rb
+++ app/helpers/my_account/application_helper.rb
@@ -1,4 +1,45 @@
 module MyAccount
   module ApplicationHelper
+
+    def method_missing method, *args, &block
+      if method.to_s.end_with?('_path') || method.to_s.end_with?('_url')
+        Rails.logger.debug "mjc12test: checking 1 for #{method.to_s}"
+        if main_app.respond_to?(method)
+          main_app.send(method, *args)
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to?(method,include_all=false)
+      if method.to_s.end_with?('_path') || method.to_s.end_with?('_url')
+        Rails.logger.debug "mjc12test: checking 2 for #{method.to_s}"
+
+        if main_app.respond_to?(method)
+          true
+        else
+          super
+        end
+      else
+        super
+      end
+    end
+
+    def part_of_catalog?
+      if params[:controller] =='catalog' || params[:controller]=='bookmarks' ||
+        request.original_url.include?("request") || params[:controller]=='search_history' ||
+        params[:controller] == 'advanced_search' || params[:controller]=='aeon' || params[:controller]=='browse' ||
+        params[:controller] == 'book_bags'
+        return true
+      end
+    end
+
+    def render_solr_core
+    
+    end
+
   end
 end
