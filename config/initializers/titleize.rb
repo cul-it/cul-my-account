2520f577ce26b082eaeba2a630277368229d5442
Matt Connolly <mjc12@cornell.edu>
2018-11-07 11:00:13 -0500

    Extend String.titleize method to try to sensibly capitalize titles

---
+++ config/initializers/titleize.rb
@@ -0,0 +1,12 @@
+# Following a suggestion from
+# https://stackoverflow.com/questions/15078964
+# TODO: This needs some work. Some characters in titles are being stripped out - like colons and dashes;
+# also, titles that *start* with excluded words may end up lowercased (speculation). As will acronyms.
+class String
+  def titleize(options = {:exclude => ["and", "or", "the", "to", "a", "but", "at", "of", "on", "in"]})
+    exclusions = options[:exclude]
+
+    return ActiveSupport::Inflector.titleize(self) unless exclusions.present?
+    self.underscore.humanize.gsub(/\b(?<!['’`])(?!(#{exclusions.join('|')})\b)[a-z]/) { $&.capitalize }
+  end
+end
