8edbeb3f501b0999d32873dc46a74bcd66d66bc1
Matt Connolly <mjc12@cornell.edu>
2019-06-11 15:57:27 -0400

    Clean up code

--- app/helpers/my_account/my_account_helper.rb
+++ app/helpers/my_account/my_account_helper.rb
@@ -6,9 +6,6 @@ module MyAccount
       #  BD has 'AllowRenew'
       #  ILL has 'ra'
       #  Voyager has ?
-      Rails.logger.debug "mjc12test: item #{item}"
-      Rails.logger.debug "mjc12test: testing renew #1 #{item[:AllowRenew]}"
-      Rails.logger.debug "mjc12test: testing renew #2 #{item[:ra]}"
       !((item['AllowRenew'] && item['AllowRenew'] == false) ||
         (item['ra'] && item['ra'] == 'No'))
     end
