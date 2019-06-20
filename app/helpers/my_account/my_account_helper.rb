f3ebc9ee6d9bd11b796e12090705cf0566fef85e
Matt Connolly <mjc12@cornell.edu>
2019-06-20 10:25:19 -0400

    Remove unused helper function

--- app/helpers/my_account/my_account_helper.rb
+++ app/helpers/my_account/my_account_helper.rb
@@ -1,14 +1,5 @@
 module MyAccount
   module MyAccountHelper
 
-    def isRenewable?(item)
-      # Different sources use different indicators for this (why?)
-      #  BD has 'AllowRenew'
-      #  ILL has 'ra'
-      #  Voyager has ?
-      !((item['AllowRenew'] && item['AllowRenew'] == false) ||
-        (item['ra'] && item['ra'] == 'No'))
-    end
-
   end
 end
